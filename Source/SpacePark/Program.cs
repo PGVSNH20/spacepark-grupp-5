7bdb1d9b698931115d34de0d68588cd626d1c76c
Isak Kavallin Johansson <79911856+isakKavallin@users.noreply.github.com>
2021-03-18 14:38:23 +0100

    commit

---
+++ Source/SpacePark/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using RestSharp;
+using System.Threading.Tasks;
+using RestSharp.Authenticators;
+
+namespace SpacePark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var client = new RestClient("https://swapi.dev/api/");
+            var request = new RestRequest("people/", DataFormat.Json);
+            // NOTE: The Swreponse is a custom class which represents the data returned by the API, RestClient have buildin ORM which maps the data from the reponse into a given type of object
+        }
+    }
+}
