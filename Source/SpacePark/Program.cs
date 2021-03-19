3536e8e6cb43a5800476998a88c89479f15bfb76
hampussle <hlinderstam@gmail.com>
2021-03-19 11:08:55 +0100

    API Test

--- Source/SpacePark/Program.cs
+++ Source/SpacePark/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using RestSharp;
-using System.Threading.Tasks;
-using RestSharp.Authenticators;
+using RestSharp.Serialization.Json;
 
 namespace SpacePark
 {
@@ -9,21 +8,44 @@ namespace SpacePark
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Rest rest = new Rest();
 
-            var client = new RestClient("https://swapi.dev/api/");
-            var request = new RestRequest("people/", DataFormat.Json);
-            var response = client.Get(request);
+            //Söker för innehåll r2
+            var R2D2request = "people/?search=r2";
+            var searchresponse = rest.StarWarsApiRequest(R2D2request);
 
-            JsonSerializer json = new JsonSerializer(typeof(Person));
-            json.Deserialize(response);
+            //Skapa en dictionairy av json-svaret
+            var person = rest.Deserialize<dynamic>(searchresponse);
+            var results = person["results"][0];
 
-            
-
-            Console.WriteLine();
+            //Skriv ut namn från dictionary, borde vara R2D2
+            Console.WriteLine(results["name"]);
+            Console.ReadLine();
             
             // Live Share Test
             // NOTE: The Swreponse is a custom class which represents the data returned by the API, RestClient have buildin ORM which maps the data arom the reponse into a given type of object
+        }
+    }
+    public class Rest
+    {
+        public T Deserialize<T>(IRestResponse response)
+        {
+
+            var deser = new JsonSerializer();
+            var person = deser.Deserialize<T>(response);
+            return person;
+        }
+
+        public IRestResponse StarWarsApiRequest(string req)
+        {
+
+            var client = new RestClient("https://swapi.dev/api/");
+            var request = new RestRequest(req, DataFormat.Json);
+            var response = client.Get(request);
+            return response;
+
+            //var request = new RestRequest("people/", DataFormat.Json);
+            //var response = client.Get(request);
         }
     }
 }
